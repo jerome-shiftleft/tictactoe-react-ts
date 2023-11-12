a7a86fd98dab84fb504be911fb0944072be05efe
jerome-shiftleft <jerome@theshiftleft.com>
2023-11-12 21:12:18 +0800

    useState isEditing

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,9 +1,14 @@
+import {useState} from 'react';
+
 type Props = {
   name: string | undefined;
   symbol: string;
 }
 
 const Player = ({name, symbol}: Props) => {
+
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+
   return (
     <li>
       <span className="player-info">
