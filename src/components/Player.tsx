55b9a60fb49676c3a335546504c0b6d39876a8d8
jerome-shiftleft <jerome@theshiftleft.com>
2023-11-12 22:30:31 +0800

    progress

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, ReactNode } from "react";
 
 type Props = {
   name: string;
@@ -7,27 +7,33 @@ type Props = {
 
 const Player = ({ name, symbol }: Props) => {
   const [isEditing, setIsEditing] = useState<boolean>(false);
-  const [playerName, setPlayerName] = useState<string>(name);
+  //const [playerName, setPlayerName] = useState<string>(name);
 
   const editPlayer = () => {
     setIsEditing(true);
     console.log("isEditing: ", isEditing);
   };
-  
+
   const savePlayer = () => {
     setIsEditing(false);
+  };
+
+  let playerName: ReactNode = <span className="player-name">{name}</span>;
+  let actionButton: ReactNode = <button onClick={editPlayer}>Edit</button>;
+
+  if (isEditing) {
+    playerName = <input type="text" value={name} />;
+    actionButton = <button onClick={savePlayer}>Save</button>;
   }
 
   return (
     <li>
       <span className="player-info">
-        {isEditing ? <input type="text" value={playerName} /> : <span className="player-name">{playerName}</span>}
-
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      
-      {isEditing ? (<button onClick={savePlayer}>Save</button>) : (<button onClick={editPlayer}>Edit</button>)}
-      
+
+      {actionButton}
     </li>
   );
 };
