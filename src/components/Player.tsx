6b36549e14bd718bd8ad836c3969d09bf591383b
jerome-shiftleft <jerome@theshiftleft.com>
2023-11-12 21:42:24 +0800

    exercise progress

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,25 +1,33 @@
 import { useState } from "react";
 
 type Props = {
-  name: string | undefined;
+  name: string;
   symbol: string;
 };
 
 const Player = ({ name, symbol }: Props) => {
   const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [playerName, setPlayerName] = useState<string>(name);
 
   const editPlayer = () => {
     setIsEditing(true);
-    console.log('isEditing: ', isEditing);
+    console.log("isEditing: ", isEditing);
+  };
+  
+  const savePlayer = () => {
+    setIsEditing(false);
   }
 
   return (
     <li>
       <span className="player-info">
-        <span className="player-name">{name}</span>
+        {isEditing ? <input type="text" value={playerName} /> : <span className="player-name">{playerName}</span>}
+
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={() => editPlayer()}>Edit</button>
+      
+      {isEditing ? (<button onClick={savePlayer}>Save</button>) : (<button onClick={editPlayer}>Edit</button>)}
+      
     </li>
   );
 };
