a64f4e7ac1d4004fedc1b4134d074e0bb060600f
jerome-shiftleft <jerome@theshiftleft.com>
2023-11-15 04:57:03 +0800

    disabling button

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -35,7 +35,7 @@ const GameBoard = ({ onSelectSquare, turns }: GameBoardProps) => {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
